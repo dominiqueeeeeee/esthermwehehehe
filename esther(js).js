d47cc7eb74884323dd35241e28d1e6f02210b53b
dominiqueeeeeee <jermainefigura55@gmail.com>
2026-02-09 22:45:35 +0800

    Add files via upload

---
+++ esther(js).js
@@ -0,0 +1,22 @@
+const cacheName = 'valentine-v1';
+const filesToCache = [
+  '/valentine.html',
+  '/manifest.json',
+  // Add other assets like images if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
